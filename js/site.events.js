9a389cef8afa342d58b5b878a4b1156e54ca70fb
Maydson Hedlund <maydson.hedlund@ficodigital.com.br>
2023-04-04 17:51:19 -0300

    ...

--- js/site.events.js
+++ js/site.events.js
@@ -1,4 +1,33 @@
 
+
+//function handleFile(files){
+//    const reader = new FileReader();
+//    reader.onload = (event) => {
+//        let data = event.target.result;
+//        document.querySelector("#texto").value = data; 
+//    };
+//    reader.readAsText(files[0]);
+//}
+//
+//
+//function LoadPartial(params) {    
+//    try {        
+//        
+//        //var body = document.getElementsByClassName(".render-body");
+//        //body.innerHTML = "teste";
+//        //console.log(body);
+//        $('.render-body').load('modalMensagem.html');
+//    } catch (error) {    
+//        console.log(error)
+//    }
+//}
+
+//window.addEventListener('load',
+//
+//);
+
+
+
 const _BTN_MODAL_MENSAGEM_  = document.getElementById("btnModal");
 
 
--- js/site.functions.js
+++ js/site.functions.js
@@ -1,17 +1,19 @@
 const _SITE_ = {}
 
-_SITE_.AbrirModalSite = function (params) {
-    
-try {
-    
-    $("#txtTextoMensagem").text(params);
-    let modal = new bootstrap.Modal(document.getElementById('modalMensagem'));
-    modal.show();
-        
-} catch (error) {    
-
-    console.log(error)
+_SITE_.AbrirModalSite = function (params) {    
+    try {        
+        $("#txtTextoMensagem").text(params);
+        let modal = new bootstrap.Modal(document.getElementById('modalMensagem'));
+        modal.show();            
+    } catch (error) {    
+        console.log(error)
+    }
 }
 
-
+_SITE_.LoadPartial = function (params) {    
+    try {        
+        console.log(params)
+    } catch (error) {    
+        console.log(error)
+    }
 }
