d89a11f7a660a838e60cceb833a4f2e04fd394cf
Maydson Hedlund <maydson.hedlund@ficodigital.com.br>
2023-04-05 14:19:12 -0300

    ...

--- js/site.events.js
+++ js/site.events.js
@@ -9,32 +9,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
         _SITE_.AbrirModalSite("Olá Mundo");
     });
 
+
     var menuFooter = document.getElementsByClassName("menu-footer");
 
-    var myFunction = function(element) {
-        //var attribute = this.getAttribute("data-myattribute");
-        alert(element);
-    };
-
     for (var i = 0; i < menuFooter.length; i++) {
-        
+
         menuFooter[i].addEventListener('click', function() {
-            
-    
-            for (var i = 0; i < menuFooter.length; i++) {        
+
+            for (var i = 0; i < menuFooter.length; i++) {
                 menuFooter[i].classList.remove("menu-activate");
-                menuFooter[i].classList.remove("heading1");
+                menuFooter[i].classList.remove("heading");
                 menuFooter[i].classList.add("menu-deactivate");
             }
             
-            
-            this.classList.add("menu-activate");
-            this.classList.add("heading1");
-            //console.log(menuFooter.classList.c);
-            //alert(this.classList.contains("foo"));
-            //menuFooter.classList.add("menu-deactivate");
+            let classElementTarget = this.classList[0].replace("-heading","");
+
+            let element = document.getElementsByClassName(classElementTarget)[0];
+
+            element.classList.remove("menu-deactivate");
+            element.classList.add("menu-activate");
+            document.getElementsByClassName(element.classList[0] + "-heading")[0].classList.add("heading")
+            document.getElementsByClassName(element.classList[0] + "-heading")[0].classList.remove("menu-activate")
+            document.getElementsByClassName(element.classList[0] + "-heading")[0].classList.remove("menu-deactivate")
+
         });
-
-
     }
 });
